66761513c814505901994444fd578e3710c04b67
luizdias01 <luizdias01@prof.educacao.sp.gov.br>
2025-08-26 12:44:33 +0000

    adiciona rotas e views do usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('cadastrar/', views.cadastrar, name='usuario_cadastrar'),
+    path('login/', views.login, name='usuario_login'),
+    path('logout/', views.logout, name='usuario_logout'),
+    path('atualizar/', views.atualizar, name='usuario_atualizar'),
+    path('exibir/', views.exibir, name='usuario_exibir'),
+    path('excluir/', views.excluir, name='usuario_excluir'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def cadastrar(request):
     pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def atualizar(request):
+    pass
+
+def exibir(request):
+    pass
+
+def excluir(request):
+    pass
